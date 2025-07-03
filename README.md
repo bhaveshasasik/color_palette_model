194a5f44bf788ce87675bac8d0dc10fb142546c1
bhaveshasasik <113221407+bhaveshasasik@users.noreply.github.com>
2025-07-02 21:42:15 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,32 @@
+# Model for the color palette generator app
+
+This is our model for the app - Color Flix
+
+## Used technologies and libraries
+* Python
+* Flask
+* sci-kit learn
+* Open CV
+* Collections
+
+## Our app
+* [ColorFlix - Color Palette Generator App](https://github.com/bhaveshasasik/color-palette-app)
+
+## Our UC Davis GDSC End-Of-Year Showcase Slides with Demo:
+* [Team Janus - ColorFlix](https://www.canva.com/design/DAFkoq4QwBo/IMwAPqqkWlxnqC_bjwB7uA/edit?utm_content=DAFkoq4QwBo&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton)
+
+## Installation/Prerequisites
+
+* Must have Python environment set up with Flask, sklearn, and Open CV downloaded.
+  * You can in most cases run pip install followed by library name on your terminal 
+  * pip install Flask, pip install scikit-learn, pip install opencv-python
+
+## Collaborators
+* Suyash Goel - https://github.com/suyashgoel
+* Bhavesha Sasikumar - https://github.com/bhaveshasasik
+* Minji Yun - https://github.com/minjiyun02
+
+## References and resources 
+
+* [Image color identification with Machine Learning and Image Processing, using Python](https://towardsdatascience.com/image-color-identification-with-machine-learning-and-image-processing-using-python-f3dd0606bdca)
+* [How to upload image from Flutter to Flask server | Build Developers | Flutter, Python & Flask](https://www.youtube.com/watch?v=Su_qbc98xsE)
---
+++ main.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, jsonify
+import werkzeug
+import sklearn
+from sklearn.cluster import MiniBatchKMeans
+from collections import Counter
+import cv2
+
+app = Flask(__name__)
+
+def get_image(image_path):
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return image
+
+def RGB2HEX(color):
+    return "#{:02x}{:02x}{:02x}".format(int(color[0]), int(color[1]), int(color[2]))
+
+def predict_colors(image_path, number_of_colors):
+    image = get_image(image_path)
+    modified_image = image.reshape(image.shape[0]*image.shape[1], 3)
+
+    clf = MiniBatchKMeans(n_clusters=number_of_colors).fit(modified_image)
+    labels = clf.fit_predict(modified_image)
+
+    counts = Counter(labels)
+    center_colors = clf.cluster_centers_
+    ordered_colors = [center_colors[i] for i in counts.keys()]
+    hex_colors = [RGB2HEX(ordered_colors[i]) for i in counts.keys()]
+
+    return hex_colors
+
+@app.route('/upload', methods=["POST"])
+def upload():
+    if request.method == "POST":
+        imageFile = request.files['image']
+        fileName = werkzeug.utils.secure_filename(imageFile.filename)
+        imageFile.save("./uploadedimages/" + fileName)
+
+        # Set the desired number of colors
+        number_of_colors = 6
+
+        # Predict colors
+        image_path = "./uploadedimages/" + fileName
+        predicted_colors = predict_colors(image_path, number_of_colors)
+
+        return jsonify({
+            "colors": predicted_colors
+        })
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
+
+
